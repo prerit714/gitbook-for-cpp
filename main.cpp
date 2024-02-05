c66fdbfb1ddbc747c08495c7c65f2558faf22cff
Prerit Dayal <62067658+prerit714@users.noreply.github.com>
2024-02-05 10:57:21 +0530

    Define templatized version of a function

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include <cstdlib>
-#include <string>
 #include <iostream>
+#include <vector>
+
+/**
+ * NOTE: Below is how you define a templatized version of a function
+ * */
+
+template <typename T> void out_vector(const std::vector<T> &v) {
+  for (const T &x : v) {
+    std::cout << x << ' ';
+  }
+  std::cout << std::endl;
+}
 
 int main() {
 
-  std::string hello_world{"Hello World"};
-  std::cout << hello_world << std::endl;
+  std::vector<int> v{1, 2, 3, 4, 5};
+  out_vector<int>(v);
 
   return EXIT_SUCCESS;
 }
