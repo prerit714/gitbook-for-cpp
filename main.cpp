f5645d35a39b6f59cd308708b21f2d14747d992b
Prerit Dayal <62067658+prerit714@users.noreply.github.com>
2024-02-05 10:12:09 +0530

    A hello world c++ file

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <cstdlib>
+#include <string>
+#include <iostream>
+
+int main() {
+
+  std::string hello_world{"Hello World"};
+  std::cout << hello_world << std::endl;
+
+  return EXIT_SUCCESS;
+}
